d654896fa69020783a1520956bc4fcff05e94016
Vidit <viditgoel2k@gmail.com>
2023-01-31 16:23:25 +0530

    logo added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,9 +6,9 @@ import {Home, CreatePost } from './pages'
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between items-center  bg-slate-800 sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={logo} alt="logo" className='w-28 object-contain' />
+          <img src={logo} alt="logo" className=' w-44 object-contain' />
         </Link>
 
         <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
